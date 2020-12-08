376c7783b74ec54f35d16f9a66f36ad90a4edbe3
Dzirt2006 <Dzirt-2006@mail.ru>
2020-12-08 15:57:23 -0500

    add to cart works correctly

--- src/test/java/pages/AddToCartPage.java
+++ src/test/java/pages/AddToCartPage.java
@@ -8,38 +8,35 @@ import org.openqa.selenium.interactions.Actions;
 
 import frameworks.Waiters;
 
-public class AddToCartPage extends BasePage implements Waiters{
-	String url="/index.php?id_category=5&controller=category";
-	
-	public AddToCartPage(String url, WebDriver driver) {
-		super(url, driver);
-	}
-	
-	public AddToCartPage navigateTo() {
-		super.navigateTo(url);
-		return this;
-	}
-	
-	public AddToCartPage hoverItem() {
-		((JavascriptExecutor) driver).executeScript("window.scrollBy(0,500)");
-		WebElement item = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li/div/div[1]/div/a[1]/img"));
-		item.click();
-		waitVisibilityId(driver, "add_to_cart");
-		return this;
-	}
-	
-	public AddToCartPage clickAddCart() {
-//		waitForClickableByXpath(driver, "//*[@id=\"add_to_cart\"]/button").click();
-		WebElement base = driver.findElement(By.xpath("//*[@id=\"product\"]/div"));
-		base.findElement(By.xpath("//*[@id=\\\"add_to_cart\\\"]/button")).click();
-		
-		return this;
-	}
-	
-	public String getString() {
-		return driver.findElement(By.className("h2 i.icon-ok")).getText();
-	}
-	
-	
+public class AddToCartPage extends BasePage implements Waiters {
+    String url = "/index.php?id_category=5&controller=category";
+
+    public AddToCartPage(String url, WebDriver driver) {
+        super(url, driver);
+    }
+
+    public AddToCartPage navigateTo() {
+        super.navigateTo(url);
+        return this;
+    }
+
+    public AddToCartPage hoverItem() {
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,500)");
+        WebElement item = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li/div/div[1]/div/a[1]/img"));
+        item.click();
+        return this;
+    }
+
+    public AddToCartPage clickAddCart() {
+        WebElement base = driver.findElement(By.tagName("iframe"));
+        driver.switchTo().frame(base);
+        driver.findElement(By.xpath("//*[@id='add_to_cart']/button")).click();
+        return this;
+    }
+
+    public String getString() {
+        return waitVisibilityXPath(driver, "//div[@id='layer_cart']/div[1]/div[1]/h2").getText();
+    }
+
 
 }
